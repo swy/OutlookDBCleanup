00bbacf80198f6e9b709d10d78c1dbb1fb3dbc10
Steve Yuroff <syuroff@hiebing.com>
2016-10-10 11:11:59 -0500

    Update1

--- OldOutlookCleanup.sh
+++ OldOutlookCleanup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 consoleUser=$(/usr/bin/stat -f %Su '/dev/console')
 homeDir=$(/usr/bin/dscl . -read "/Users/${consoleUser}" NFSHomeDirectory | /usr/bin/awk -F ': ' '{print $2}')
-startDir="${homeDir}/Documents/Microsoft User Data/Office 2011 Identities/"
-oldDatabases+=$(/usr/bin/find "${startDir}" -name "Database" -type f -mtime +4300 | cut -c 2-)
-for eachDatabase in "${oldDatabases[@]}"; do
-	echo "Removing $eachDatabase"
+startDir="${homeDir}/Documents/Microsoft User Data/Office 2011 Identities"
+
+echo "Starting directory is $startDir"
+
+oldDatabases+=$(/usr/bin/find "${startDir}" -name "Database" -type f -mtime -3 | cut -c 2- | rev | cut -d '/' -f 2 | rev)
+for eachDatabase in "${oldDatabases}"; do
+	echo "This will remove ${startDir}/${eachDatabase}"
 #	rm -Rfv "${startDir}/${eachDatabase}"
 done
 exit 0
