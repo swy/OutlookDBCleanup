7a10d6a665a0bd2260476bb8ecb955460a6b4b8e
Steve Yuroff <syuroff@hiebing.com>
2016-10-10 13:20:57 -0500

    still not there yet

--- OldOutlookCleanup.sh
+++ OldOutlookCleanup.sh
@@ -5,9 +5,10 @@ startDir="${homeDir}/Documents/Microsoft User Data/Office 2011 Identities"
 
 echo "Starting directory is $startDir"
 
-oldDatabases+=$(/usr/bin/find "${startDir}" -name "Database" -type f -mtime -3 | cut -c 2- | rev | cut -d '/' -f 2 | rev)
-for eachDatabase in "${oldDatabases}"; do
-	echo "This will remove ${startDir}/${eachDatabase}"
-#	rm -Rfv "${startDir}/${eachDatabase}"
+oldDatabases+=$(/usr/bin/find "${startDir}" -name "Database" -type f | cut -c 2- | rev | cut -d '/' -f 2 | rev)
+for matchingDatabase in "${oldDatabases[@]}"; do
+	echo matchingDatabase is "${matchingDatabase}"
+	echo "This will remove ${startDir}/${matchingDatabase}"
+#	rm -Rfv "${startDir}/${matchingDatabase}"
 done
 exit 0
